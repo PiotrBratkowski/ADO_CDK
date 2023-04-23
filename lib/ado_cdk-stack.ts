3cc10bf945c70d12e31d574e186e093d75182c7d
Piotr Bratkowski <Bratek@localhost.localdomain>
2023-04-23 09:25:54 +0200

    Cleaned

--- lib/ado_cdk-stack.ts
+++ lib/ado_cdk-stack.ts
@@ -2,18 +2,35 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class AdoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AdoCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+      const hello = new lambda.Function(this,'HelloHandler', {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        code: lambda.Code.fromAsset('lambda'),
+        handler: 'hello.handler'
+      });
+
+
+      const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+        downstream: hello
+      });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
     });
 
-    const topic = new sns.Topic(this, 'AdoCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    });
   }
 }
